fd5538ed40a565ce6f16b9c27b38df625771e550
Jared Phillips <jared.phillips@marquette.edu>
2019-10-30 15:37:21 -0500

    drawWave function working

--- Source.cpp
+++ Source.cpp
@@ -46,18 +46,38 @@ void drawDot(GLint x, GLint y)
 
 void drawWave() {
 	//Sleep(1000);
-	glColor3f(1.0f, 1.0f, 0.0f);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glLineWidth(3.0);
+	for (int c = 0; c <= 100; c += 10) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_LINE_STRIP);
+		for (GLfloat x = 0; x < screenWidth; x += 0.1)     // draw the plot
+			glVertex2f(c+ 25 * x, 400 + 10 * sin(pi * x));
+		glEnd();
+		glFlush();
+		Sleep(1000);
 
-	glRecti(20, 20, 100, 100);
-	//glBegin(GL_LINE_STRIP);
-	//for (GLfloat x = 0; x < screenWidth; x++)     // draw the plot
-		//glVertex2f(x, 400+20*sin(pi * x));
-	glEnd();
-	glFlush();
+	}
 }
 
 void drawHealthBar(int health) {
 	cout << "Health";
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(0.8f, 0.1f, 0.0f);
+	glRecti(screenWidth - 110, screenHeight - 40, screenWidth - 110 + health, screenHeight - 10);
+	glFlush();
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glLineWidth(3.0);
+	glBegin(GL_LINE_LOOP);
+	glVertex2i(screenWidth - 110, screenHeight - 40);
+	glVertex2i(screenWidth - 110, screenHeight - 10);
+	glVertex2i(screenWidth - 10, screenHeight - 10);
+	glVertex2i(screenWidth - 10, screenHeight - 40);
+	glEnd();
+	glFlush();
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glutStrokeCharacter(GLUT_STROKE_ROMAN, 'H');
 }
 
 void moveCharacter(int x, int y) {
@@ -102,9 +122,11 @@ void myMouse(int button, int state, int x, int y) {
 
 void myDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	drawWave();
-	//drawHealthBar(health);
-	glFlush();
+	//glMatrixMode(GL_MODELVIEW);
+	//glLoadIdentity();
+	//drawWave();
+	drawHealthBar(health);
+	//glFlush();
 }
 
 int main(int argc, char** argv) {
